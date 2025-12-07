6fd3cc87be5314d1609496ae0f685afd7b3bdad7
DominicDev <s0l0estudi4r@gmail.com>
2025-12-07 19:10:49 +0100

    v0.0.1

---
+++ app/accordion/page.tsx
@@ -0,0 +1,9 @@
+import { AccordionDemo } from '@/components/myComponents/accordion';
+
+export default function Page() {
+  return (
+    <article className='page'>
+      <AccordionDemo />
+    </article>
+  );
+}
