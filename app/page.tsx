0066271b1af97e2d3f5803142d1e99e320a72244
DominicDev <s0l0estudi4r@gmail.com>
2025-12-29 13:53:59 +0100

    v0.2.3

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,24 @@
 import Link from 'next/link';
 
 const linksToComponents = [
-  { title: 'Accordion' , href: 'accordion'},
-  { title: 'Alert Dialog' , href: 'alert-dialog'},
-  { title: 'Buttons' , href: 'buttons'},
-  { title: 'Dialog Share Link' , href: 'dialog-share-link'},
-  { title: 'Dialog Input' , href: 'dialog-input'},
-  { title: 'Badge' , href: 'badge'},
-  { title: 'Simple Login Form' , href: 'simple-login-form'},
-  { title: 'Calendar' , href: 'calendar'},
-  { title: 'Multiple Calendar Select' , href: 'multiple-calendar-select'},
-  { title: 'Avatar' , href: 'avatar'},
-  { title: 'Card Login' , href: 'card-login'},
-  { title: 'Carousel 1x1 + Loop' , href: 'carousel-1x1'},
-  { title: 'Carousel 1x5' , href: 'carousel-1x5'},
-  { title: 'Carousel Vertical + Autoplay' , href: 'carousel-vertical'},
-  { title: 'Checkbox' , href: 'checkbox'},
-  { title: 'Command' , href: 'command'},
-  { title: 'Command Dialog' , href: 'command-dialog'},
+  { title: 'Accordion', href: 'accordion' },
+  { title: 'Alert Dialog', href: 'alert-dialog' },
+  { title: 'Buttons', href: 'buttons' },
+  { title: 'Dialog Share Link', href: 'dialog-share-link' },
+  { title: 'Dialog Input', href: 'dialog-input' },
+  { title: 'Badge', href: 'badge' },
+  { title: 'Simple Login Form', href: 'simple-login-form' },
+  { title: 'Calendar', href: 'calendar' },
+  { title: 'Multiple Calendar Select', href: 'multiple-calendar-select' },
+  { title: 'Avatar', href: 'avatar' },
+  { title: 'Card Login', href: 'card-login' },
+  { title: 'Carousel 1x1 + Loop', href: 'carousel-1x1' },
+  { title: 'Carousel 1x5', href: 'carousel-1x5' },
+  { title: 'Carousel Vertical + Autoplay', href: 'carousel-vertical' },
+  { title: 'Checkbox', href: 'checkbox' },
+  { title: 'Command', href: 'command' },
+  { title: 'Command Dialog', href: 'command-dialog' },
+  { title: 'Popover', href: 'popover' }
 ];
 
 export default function Home() {
