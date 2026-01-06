8e8b32590539f511e92789d48e6950a27ff091bf
DominicDev <s0l0estudi4r@gmail.com>
2026-01-06 16:02:40 +0100

    v0.3.8

--- app/sonner/page.tsx
+++ app/sonner/page.tsx
@@ -1,14 +1,16 @@
 import { PageContainer } from '@/components/myComponents/body';
 import { ImageBackground } from '@/components/myComponents/image-background';
+import { SonnerDemo } from './SonnerDemo';
 
 export const metadata = {
-  title: ''
+  title: 'Sonner'
 };
 
 export default function Page() {
   return (
     <PageContainer>
       <ImageBackground src='/assets/sonner-bg.svg' />
+      <SonnerDemo />
     </PageContainer>
   );
 }
