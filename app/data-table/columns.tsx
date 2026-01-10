a1a80431144ce9d6ab8d9e8f2525fbf1d643058b
DominicDev <s0l0estudi4r@gmail.com>
2026-01-10 20:13:13 +0100

    v0.4.2

--- app/data-table/columns.tsx
+++ app/data-table/columns.tsx
@@ -1,27 +1,27 @@
-"use client";
+'use client';
 
-import { Badge } from "@/components/ui/badge";
-import { Payment } from "@/data/payments.data";
-import { ColumnDef, FilterFn, Row, SortDirection } from "@tanstack/react-table";
+import { Badge } from '@/components/ui/badge';
+import { Payment } from '@/app/data-table/data/payments.data';
+import { ColumnDef, FilterFn, Row, SortDirection } from '@tanstack/react-table';
 
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
 
-import { Checkbox } from "@/components/ui/checkbox";
+import { Checkbox } from '@/components/ui/checkbox';
 
 import {
   ChevronDownIcon,
   ChevronUpIcon,
-  DotsHorizontalIcon,
-} from "@radix-ui/react-icons";
-import { toast } from "sonner";
+  DotsHorizontalIcon
+} from '@radix-ui/react-icons';
+import { toast } from 'sonner';
 
 // import { toast } from "@/components/ui/use-toast";
 
@@ -33,20 +33,20 @@ const myCustomFilterFn: FilterFn<Payment> = (
 ) => {
   filterValue = filterValue.toLowerCase();
 
-  const filterParts = filterValue.split(" ");
+  const filterParts = filterValue.split(' ');
   const rowValues =
     `${row.original.email} ${row.original.clientName} ${row.original.status}`.toLowerCase();
 
-  return filterParts.every((part) => rowValues.includes(part));
+  return filterParts.every(part => rowValues.includes(part));
 };
 
 const SortedIcon = ({ isSorted }: { isSorted: false | SortDirection }) => {
-  if (isSorted === "asc") {
-    return <ChevronUpIcon className="h-4 w-4" />;
+  if (isSorted === 'asc') {
+    return <ChevronUpIcon className='h-4 w-4' />;
   }
 
-  if (isSorted === "desc") {
-    return <ChevronDownIcon className="h-4 w-4" />;
+  if (isSorted === 'desc') {
+    return <ChevronDownIcon className='h-4 w-4' />;
   }
 
   return null;
@@ -56,48 +56,48 @@ const SortedIcon = ({ isSorted }: { isSorted: false | SortDirection }) => {
 // You can use a Zod schema here if you want.
 export const columns: ColumnDef<Payment>[] = [
   {
-    id: "select",
+    id: 'select',
     header: ({ table }) => (
       <Checkbox
         checked={
           table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
+          (table.getIsSomePageRowsSelected() && 'indeterminate')
         }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
+        onCheckedChange={value => table.toggleAllPageRowsSelected(!!value)}
+        aria-label='Select all'
       />
     ),
     cell: ({ row }) => (
       <Checkbox
         checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
+        onCheckedChange={value => row.toggleSelected(!!value)}
+        aria-label='Select row'
       />
     ),
     enableSorting: false,
-    enableHiding: false,
+    enableHiding: false
   },
   {
-    accessorKey: "clientName",
+    accessorKey: 'clientName',
     header: ({ column }) => {
       return (
         <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
         >
           Client Name
           <SortedIcon isSorted={column.getIsSorted()} />
         </Button>
       );
-    },
+    }
   },
   {
-    accessorKey: "status",
+    accessorKey: 'status',
     header: ({ column }) => {
       return (
         <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
         >
           Status
           <SortedIcon isSorted={column.getIsSorted()} />
@@ -105,30 +105,30 @@ export const columns: ColumnDef<Payment>[] = [
       );
     },
     cell: ({ row }) => {
-      const status = row.getValue("status") as string;
+      const status = row.getValue('status') as string;
       const variant =
         {
-          pending: "secondary",
-          processing: "info",
-          success: "success",
-          failed: "destructive",
-        }[status] ?? ("default" as any);
+          pending: 'secondary',
+          processing: 'info',
+          success: 'success',
+          failed: 'destructive'
+        }[status] ?? ('default' as any);
 
       return (
         <Badge variant={variant} capitalize>
           {status}
         </Badge>
       );
-    },
+    }
   },
   {
-    accessorKey: "amount",
+    accessorKey: 'amount',
     header: ({ column }) => {
       return (
-        <div className="text-right">
+        <div className='text-right'>
           <Button
-            variant="ghost"
-            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            variant='ghost'
+            onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
           >
             Amount
             <SortedIcon isSorted={column.getIsSorted()} />
@@ -138,52 +138,52 @@ export const columns: ColumnDef<Payment>[] = [
     },
     // header: () => <div className="text-right">Amount</div>,
     cell: ({ row }) => {
-      const amount = parseFloat(row.getValue("amount"));
-      const formatted = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
+      const amount = parseFloat(row.getValue('amount'));
+      const formatted = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
       }).format(amount);
 
-      return <div className="text-right font-medium">{formatted}</div>;
-    },
+      return <div className='text-right font-medium'>{formatted}</div>;
+    }
   },
   {
-    accessorKey: "email",
+    accessorKey: 'email',
     filterFn: myCustomFilterFn,
     header: ({ column }) => {
       return (
         <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
         >
           Email
           <SortedIcon isSorted={column.getIsSorted()} />
         </Button>
       );
-    },
+    }
   },
 
   {
-    id: "actions",
+    id: 'actions',
     cell: ({ row }) => {
       const payment = row.original;
 
       return (
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <DotsHorizontalIcon className="h-4 w-4" />
+            <Button variant='ghost' className='h-8 w-8 p-0'>
+              <span className='sr-only'>Open menu</span>
+              <DotsHorizontalIcon className='h-4 w-4' />
             </Button>
           </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
+          <DropdownMenuContent align='end'>
             <DropdownMenuLabel>Actions</DropdownMenuLabel>
             <DropdownMenuItem
               onClick={() => {
                 navigator.clipboard.writeText(payment.id);
-                toast("Payment ID copied to clipboard", {
-                  position: "top-right",
-                  duration: 3000,
+                toast('Payment ID copied to clipboard', {
+                  position: 'top-right',
+                  duration: 3000
                 });
                 // toast({
                 //   description: "Payment ID copied to clipboard",
@@ -198,6 +198,6 @@ export const columns: ColumnDef<Payment>[] = [
           </DropdownMenuContent>
         </DropdownMenu>
       );
-    },
-  },
+    }
+  }
 ];
--- app/data-table/data-table.tsx
+++ app/data-table/data-table.tsx
@@ -1,5 +1,5 @@
-"use client";
-import { useState } from "react";
+'use client';
+import { useState } from 'react';
 
 import {
   ColumnDef,
@@ -11,8 +11,8 @@ import {
   getPaginationRowModel,
   getSortedRowModel,
   useReactTable,
-  VisibilityState,
-} from "@tanstack/react-table";
+  VisibilityState
+} from '@tanstack/react-table';
 
 import {
   Table,
@@ -20,15 +20,15 @@ import {
   TableCell,
   TableHead,
   TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+  TableRow
+} from '@/components/ui/table';
 
 import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
   DropdownMenuContent,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
 
 import {
   Select,
@@ -37,12 +37,12 @@ import {
   SelectItem,
   SelectLabel,
   SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+  SelectValue
+} from '@/components/ui/select';
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import type { Payment } from "@/data/payments.data";
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import type { Payment } from '@/app/data-table/data/payments.data';
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -51,11 +51,11 @@ interface DataTableProps<TData, TValue> {
 
 export function DataTable<TData, TValue>({
   columns,
-  data,
+  data
 }: DataTableProps<TData, TValue>) {
   const [sorting, setSorting] = useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
-  const [currentStatus, setCurrentStatus] = useState("all");
+  const [currentStatus, setCurrentStatus] = useState('all');
   const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
   const [rowSelection, setRowSelection] = useState({});
 
@@ -76,62 +76,62 @@ export function DataTable<TData, TValue>({
       sorting,
       columnFilters,
       columnVisibility,
-      rowSelection,
-    },
+      rowSelection
+    }
   });
 
   return (
     <div>
-      <div className="flex items-center justify-between py-4">
+      <div className='flex items-center justify-between py-4'>
         <Input
-          placeholder="Filter anything...(client name, email, status)"
-          value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
-          onChange={(event) => {
-            setCurrentStatus("all");
-            table.getColumn("status")?.setFilterValue(undefined);
-            table.getColumn("email")?.setFilterValue(event.target.value);
+          placeholder='Filter anything...(client name, email, status)'
+          value={(table.getColumn('email')?.getFilterValue() as string) ?? ''}
+          onChange={event => {
+            setCurrentStatus('all');
+            table.getColumn('status')?.setFilterValue(undefined);
+            table.getColumn('email')?.setFilterValue(event.target.value);
           }}
-          className="max-w-sm"
+          className='max-w-sm'
         />
 
         <Select
           value={currentStatus}
-          onValueChange={(value) => {
-            if (value === "all") {
-              table.getColumn("status")?.setFilterValue(undefined);
-              setCurrentStatus("all");
+          onValueChange={value => {
+            if (value === 'all') {
+              table.getColumn('status')?.setFilterValue(undefined);
+              setCurrentStatus('all');
               return;
             }
 
             setCurrentStatus(value);
-            table.getColumn("status")?.setFilterValue(value);
+            table.getColumn('status')?.setFilterValue(value);
           }}
         >
-          <SelectTrigger className="w-[180px] ml-2">
-            <SelectValue placeholder="Status - All" />
+          <SelectTrigger className='w-[180px] ml-2'>
+            <SelectValue placeholder='Status - All' />
           </SelectTrigger>
           <SelectContent>
             <SelectGroup>
               <SelectLabel>Status</SelectLabel>
-              <SelectItem value="all">All</SelectItem>
-              <SelectItem value="pending">Pending</SelectItem>
-              <SelectItem value="processing">Processing</SelectItem>
-              <SelectItem value="success">Success</SelectItem>
-              <SelectItem value="failed">Failed</SelectItem>
+              <SelectItem value='all'>All</SelectItem>
+              <SelectItem value='pending'>Pending</SelectItem>
+              <SelectItem value='processing'>Processing</SelectItem>
+              <SelectItem value='success'>Success</SelectItem>
+              <SelectItem value='failed'>Failed</SelectItem>
             </SelectGroup>
           </SelectContent>
         </Select>
 
         {isDeleteVisible && (
           <Button
-            className="ml-2"
-            variant="destructive"
+            className='ml-2'
+            variant='destructive'
             onClick={() => {
               // table.getSelectedRowModel().rows.forEach((row) => {
               //   console.log(row.original);
               // });
 
-              const ids = table.getSelectedRowModel().rows.map((row) => {
+              const ids = table.getSelectedRowModel().rows.map(row => {
                 return (row.original as Payment).clientName;
               });
 
@@ -144,24 +144,22 @@ export function DataTable<TData, TValue>({
 
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="ml-auto">
+            <Button variant='outline' className='ml-auto'>
               Columns
             </Button>
           </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
+          <DropdownMenuContent align='end'>
             {table
               .getAllColumns()
-              .filter((column) => column.getCanHide())
-              .filter((column) => column.id !== "actions")
-              .map((column) => {
+              .filter(column => column.getCanHide())
+              .filter(column => column.id !== 'actions')
+              .map(column => {
                 return (
                   <DropdownMenuCheckboxItem
                     key={column.id}
-                    className="capitalize"
+                    className='capitalize'
                     checked={column.getIsVisible()}
-                    onCheckedChange={(value) =>
-                      column.toggleVisibility(!!value)
-                    }
+                    onCheckedChange={value => column.toggleVisibility(!!value)}
                   >
                     {column.id}
                   </DropdownMenuCheckboxItem>
@@ -171,12 +169,12 @@ export function DataTable<TData, TValue>({
         </DropdownMenu>
       </div>
 
-      <div className="rounded-md border">
+      <div className='rounded-md border'>
         <Table>
           <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
+            {table.getHeaderGroups().map(headerGroup => (
               <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
+                {headerGroup.headers.map(header => {
                   return (
                     <TableHead key={header.id}>
                       {header.isPlaceholder
@@ -193,27 +191,21 @@ export function DataTable<TData, TValue>({
           </TableHeader>
           <TableBody>
             {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
+              table.getRowModel().rows.map(row => (
                 <TableRow
                   key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
+                  data-state={row.getIsSelected() && 'selected'}
                 >
-                  {row.getVisibleCells().map((cell) => (
+                  {row.getVisibleCells().map(cell => (
                     <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
                     </TableCell>
                   ))}
                 </TableRow>
               ))
             ) : (
               <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
+                <TableCell colSpan={columns.length} className='h-24 text-center'>
                   No results.
                 </TableCell>
               </TableRow>
@@ -221,24 +213,24 @@ export function DataTable<TData, TValue>({
           </TableBody>
         </Table>
 
-        <div className="space-x-2 py-4 mx-2 flex justify-between items-center">
-          <div className="flex-1 text-sm text-muted-foreground">
-            {table.getFilteredSelectedRowModel().rows.length} of{" "}
+        <div className='space-x-2 py-4 mx-2 flex justify-between items-center'>
+          <div className='flex-1 text-sm text-muted-foreground'>
+            {table.getFilteredSelectedRowModel().rows.length} of{' '}
             {table.getFilteredRowModel().rows.length} row(s) selected.
           </div>
 
-          <div className="flex items-center justify-end space-x-2 ">
+          <div className='flex items-center justify-end space-x-2 '>
             <Button
-              variant="outline"
-              size="sm"
+              variant='outline'
+              size='sm'
               onClick={() => table.previousPage()}
               disabled={!table.getCanPreviousPage()}
             >
               Previous
             </Button>
             <Button
-              variant="outline"
-              size="sm"
+              variant='outline'
+              size='sm'
               onClick={() => table.nextPage()}
               disabled={!table.getCanNextPage()}
             >
@@ -248,21 +240,21 @@ export function DataTable<TData, TValue>({
         </div>
 
         <Select
-          onValueChange={(value) => {
+          onValueChange={value => {
             table.setPageSize(+value);
           }}
         >
-          <SelectTrigger className="w-[180px] m-2">
-            <SelectValue placeholder="10 Rows" />
+          <SelectTrigger className='w-[180px] m-2'>
+            <SelectValue placeholder='10 Rows' />
           </SelectTrigger>
           <SelectContent>
             <SelectGroup>
               <SelectLabel>Rows per page</SelectLabel>
-              <SelectItem value="10">10</SelectItem>
-              <SelectItem value="20">20</SelectItem>
-              <SelectItem value="30">30</SelectItem>
-              <SelectItem value="50">40</SelectItem>
-              <SelectItem value="100">100</SelectItem>
+              <SelectItem value='10'>10</SelectItem>
+              <SelectItem value='20'>20</SelectItem>
+              <SelectItem value='30'>30</SelectItem>
+              <SelectItem value='50'>40</SelectItem>
+              <SelectItem value='100'>100</SelectItem>
             </SelectGroup>
           </SelectContent>
         </Select>
