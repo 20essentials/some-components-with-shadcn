d75b8649928b56438f3545093994f7fb37460426
DominicDev <s0l0estudi4r@gmail.com>
2026-02-17 13:02:29 +0100

    v0.9.6

--- app/alert/page.tsx
+++ app/alert/page.tsx
@@ -1,13 +1,10 @@
 import { PageContainer } from '@/components/myComponents/body';
 import { ImageBackground } from '@/components/myComponents/image-background';
-import { AlertCircleIcon, CheckCircle2Icon, PopcornIcon } from "lucide-react"
-import {
-  Alert,
-  AlertDescription,
-  AlertTitle,
-} from "@/components/ui/alert"
+import { AlertCircleIcon, CheckCircle2Icon, PopcornIcon } from 'lucide-react';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { Metadata } from 'next';
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Alert'
 };
 
@@ -15,33 +12,33 @@ export default function Page() {
   return (
     <PageContainer>
       <ImageBackground src='/assets/background-alert.svg' />
-       <div className="grid w-full max-w-xl items-start gap-4">
-      <Alert>
-        <CheckCircle2Icon />
-        <AlertTitle>Success! Your changes have been saved</AlertTitle>
-        <AlertDescription>
-          This is an alert with icon, title and description.
-        </AlertDescription>
-      </Alert>
-      <Alert>
-        <PopcornIcon />
-        <AlertTitle>
-          This Alert has a title and an icon. No description.
-        </AlertTitle>
-      </Alert>
-      <Alert variant="destructive">
-        <AlertCircleIcon />
-        <AlertTitle>Unable to process your payment.</AlertTitle>
-        <AlertDescription>
-          <p>Please verify your billing information and try again.</p>
-          <ul className="list-inside list-disc text-sm">
-            <li>Check your card details</li>
-            <li>Ensure sufficient funds</li>
-            <li>Verify billing address</li>
-          </ul>
-        </AlertDescription>
-      </Alert>
-    </div>
+      <div className='grid w-full max-w-xl items-start gap-4'>
+        <Alert>
+          <CheckCircle2Icon />
+          <AlertTitle>Success! Your changes have been saved</AlertTitle>
+          <AlertDescription>
+            This is an alert with icon, title and description.
+          </AlertDescription>
+        </Alert>
+        <Alert>
+          <PopcornIcon />
+          <AlertTitle>
+            This Alert has a title and an icon. No description.
+          </AlertTitle>
+        </Alert>
+        <Alert variant='destructive'>
+          <AlertCircleIcon />
+          <AlertTitle>Unable to process your payment.</AlertTitle>
+          <AlertDescription>
+            <p>Please verify your billing information and try again.</p>
+            <ul className='list-inside list-disc text-sm'>
+              <li>Check your card details</li>
+              <li>Ensure sufficient funds</li>
+              <li>Verify billing address</li>
+            </ul>
+          </AlertDescription>
+        </Alert>
+      </div>
     </PageContainer>
   );
 }
